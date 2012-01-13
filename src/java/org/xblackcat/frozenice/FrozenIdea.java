5a39575b4e2b1b8a86ef08ed0a05b035c73b4406
xBlackCat <xBlackCat@localhost>
2012-01-13 15:34:23 +0200

    Fix storing plugin config

--- src/java/org/xblackcat/frozenice/FrozenIdea.java
+++ src/java/org/xblackcat/frozenice/FrozenIdea.java
@@ -3,6 +3,7 @@ package org.xblackcat.frozenice;
 import com.intellij.openapi.components.*;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
+import org.xblackcat.frozenice.config.IceConfig;
 
 /**
  * @author xBlackCat
@@ -22,6 +23,8 @@ import org.jetbrains.annotations.NotNull;
         }
 )
 public class FrozenIdea implements BaseComponent, PersistentStateComponent<Element>, ProjectComponent {
+    private IceConfig iceConfig;
+
     public FrozenIdea() {
     }
 
@@ -50,12 +53,38 @@ public class FrozenIdea implements BaseComponent, PersistentStateComponent<Eleme
         final Element root = new Element("frozen-idea");
 
         Element frameWorksList = new Element("ice-frameworks");
+        if (iceConfig != null && iceConfig.getFrameworkHomeUrl() != null) {
+            Element fw = new Element("item");
 
+            fw.setAttribute("url", iceConfig.getFrameworkHomeUrl());
+            
+            frameWorksList.addContent(fw);
+        }
+
+        root.addContent(frameWorksList);
 
         return root;
     }
 
     @Override
     public void loadState(Element state) {
+        Element fwList = state.getChild("ice-frameworks");
+        if (fwList != null) {
+            Element fw = fwList.getChild("item");
+            if (fw != null) {
+                String url = fw.getAttributeValue("url");
+                if (url != null) {
+                    iceConfig = new IceConfig(url);
+                }
+            }
+        }
+    }
+
+    public void setConfig(IceConfig iceConfig) {
+        this.iceConfig = iceConfig;
+    }
+
+    public IceConfig getConfig() {
+        return iceConfig;
     }
 }
