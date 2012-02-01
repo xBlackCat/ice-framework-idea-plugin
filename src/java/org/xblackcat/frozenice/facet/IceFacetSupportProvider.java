54294b1657a26fe611c538dede34d6d91485c25e
xBlackCat <xBlackCat@localhost>
2012-02-01 13:12:26 +0200

    A Java Ice library  detection was added to facet after a project is loaded.

--- src/java/org/xblackcat/frozenice/facet/IceFacetSupportProvider.java
+++ src/java/org/xblackcat/frozenice/facet/IceFacetSupportProvider.java
@@ -4,14 +4,22 @@ import com.intellij.facet.FacetManager;
 import com.intellij.ide.util.frameworkSupport.FrameworkSupportConfigurable;
 import com.intellij.ide.util.frameworkSupport.FrameworkSupportModel;
 import com.intellij.ide.util.frameworkSupport.FrameworkSupportProvider;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationListener;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.module.UnknownModuleType;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
 import org.jetbrains.annotations.NotNull;
 import org.xblackcat.frozenice.config.IceFrameworkSupportConfigurable;
+import org.xblackcat.frozenice.util.IceErrorMessages;
 import org.xblackcat.frozenice.util.Icons;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
 
 /**
  * 08.01.12 12:58
@@ -36,6 +44,29 @@ public class IceFacetSupportProvider extends FrameworkSupportProvider {
 
     @Override
     public boolean isSupportAlreadyAdded(@NotNull Module module) {
+        com.intellij.openapi.project.Project project = module.getProject();
+
+        JavaPsiFacade facade = project.getComponent(JavaPsiFacade.class);
+
+        PsiClass iceClass = facade.findClass("Ice.Object", module.getModuleWithDependenciesAndLibrariesScope(false));
+
+        if (iceClass == null) {
+            // No class found!
+            final Notification notification = new Notification(
+                    "Ice Facet",
+                    IceErrorMessages.message("ICE.library.not.added"),
+                    IceErrorMessages.message("ICE.library.not.added.message"),
+                    NotificationType.ERROR,
+                    new NotificationListener() {
+                        @Override
+                        public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
+                        }
+
+                    }
+            );
+            Notifications.Bus.notify(notification, project);
+        }
+
         return FacetManager.getInstance(module).getFacetByType(IceFacet.ID) != null;
     }
 
