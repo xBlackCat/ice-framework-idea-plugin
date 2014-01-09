c5b6a6279cfe7d91151b9aecb307130a8d6714b3
xBlackCat <xBlackCat@localhost>
2014-01-09 12:14:33 +0200

    Add simple icons to identify elements

--- plugin/src/java/org/xblackcat/frozenice/psi/impl/SliceNamedElementImpl.java
+++ plugin/src/java/org/xblackcat/frozenice/psi/impl/SliceNamedElementImpl.java
@@ -15,6 +15,7 @@
  */
 package org.xblackcat.frozenice.psi.impl;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.LocalSearchScope;
@@ -23,8 +24,7 @@ import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.xblackcat.frozenice.psi.SliceNamedElement;
-import org.xblackcat.frozenice.psi.SliceTypes;
+import org.xblackcat.frozenice.psi.*;
 
 import javax.swing.*;
 
@@ -77,9 +77,34 @@ public abstract class SliceNamedElementImpl extends SliceCompositeElementImpl im
         return new LocalSearchScope(getContainingFile());
     }
 
+    @Nullable
     @Override
-    public Icon getIcon(int flags) {
-        return super.getIcon(flags);
+    protected Icon getElementIcon(@IconFlags int flags) {
+        if (this instanceof SliceModule) {
+            return AllIcons.Nodes.Package;
+        } else if (this instanceof SliceMethodDef) {
+            return AllIcons.Nodes.AbstractMethod;
+        } else if (this instanceof SliceFieldDef) {
+            return AllIcons.Nodes.Field;
+        } else if (this instanceof SliceConstant) {
+            return AllIcons.Nodes.Field;
+        } else if (this instanceof SliceClassDef) {
+            return AllIcons.Nodes.Class;
+        } else if (this instanceof SliceInterfaceDef) {
+            return AllIcons.Nodes.Interface;
+        } else if (this instanceof SliceExceptionDef) {
+            return AllIcons.Nodes.ExceptionClass;
+        } else if (this instanceof SliceStructDef) {
+            return AllIcons.Nodes.AnonymousClass;
+        } else if (this instanceof SliceEnumDef) {
+            return AllIcons.Nodes.Enum;
+        } else if (this instanceof SliceDictionaryDef) {
+            return AllIcons.Nodes.Advice;
+        } else if (this instanceof SliceSequenceDef) {
+            return AllIcons.Nodes.Advice;
+        }
+
+        return super.getElementIcon(flags);
     }
 
     @Nullable
