f386f576a7dbdb0daa8d465b6f442d73990d869f
xBlackCat <xblackcat@gmail.com>
2016-03-24 16:31:55 +0300

    Update icons

--- plugin/src/java/org/xblackcat/frozenidea/util/SliceIcons.java
+++ plugin/src/java/org/xblackcat/frozenidea/util/SliceIcons.java
@@ -10,9 +10,16 @@ import javax.swing.*;
  * @author xBlackCat
  */
 public interface SliceIcons {
-    Icon FACET_ICON_32 = IconLoader.getIcon("/icons/facet-32.png");
     Icon FACET_ICON = IconLoader.getIcon("/icons/facet.png");
     Icon FILE_ICON = IconLoader.getIcon("/icons/file.png");
+
+    Icon CLASS_ICON = IconLoader.getIcon("/icons/class.png");
+    Icon EXCEPTION_ICON = IconLoader.getIcon("/icons/exceptionClass.png");
+    Icon ENUM_ICON = IconLoader.getIcon("/icons/enum.png");
+    Icon FIELD_ICON = IconLoader.getIcon("/icons/field.png");
+    Icon INTERFACE_ICON = IconLoader.getIcon("/icons/interface.png");
+    Icon METHOD_ICON = IconLoader.getIcon("/icons/method.png");
+    Icon PROPERTY_ICON = IconLoader.getIcon("/icons/property.png");
 
 
     Icon TRANSLATED = IconLoader.getIcon("/actions/compile.png");
